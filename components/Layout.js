80d1e94213ef1744013503089806acf09afc84fa
mikeisfake <mikescooper@icloud.com>
2021-06-12 05:56:47 -0400

    modifying page layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,11 @@ import Link from 'next/link'
  const Layout = ({ children }) => {
    return (
     <div className="layout">
-      <Link href="/"><a> Home </a></Link>
-      <Link href="/about"><a> About </a></Link>
+      <nav className="nav">
+        <Link href="/"><a> <h3>Home</h3> </a></Link>
+        <Link href="/about"><a> <h3>About</h3> </a></Link>
+        <Link href="/"><a> <h3>Builds</h3> </a></Link>
+      </nav>
       { children }
     </div>
 
