9dac356cf2ae4d125f4b160936a39b87a916ec47
mikeisfake <mikescooper@icloud.com>
2021-06-19 21:16:52 -0400

    begin building out fetch to contentful

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 const Home = () => {
+
+  const getStaticProps = async () => {
+    const res = await fetch(BASE_URL + process.env.CONTENTFUL_ACCESS_TOKEN)   
+  }
+
    return (
      <div className="home">
        <h1>HomePage</h1>
