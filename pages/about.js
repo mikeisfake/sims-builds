5f51a056d2d2379b96b7a3d0d93406213d5234cb
mikeisfake <mikescooper@icloud.com>
2021-07-12 04:30:28 -0400

    misc updates

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,17 @@
 const About = () => {
-   return (
-     <div className="about">
-       <h1>About Page</h1>
-       <p className="description">
-         This app is designed to show off some of my favorite builds I've made in The Sims 4. When I'm not coding my favorite activity is building houses in The Sims.<br />
-         This app was created using Next.js on the front end and the content is managed from Contenful
-       </p>
-     </div>
-   );
-  }
+  return (
+    <div className="about">
+      <h1>About Page</h1>
+      <p className="description">
+        This app is designed to show off some of my favorite builds I've made in
+        The Sims 4. When I'm not coding my favorite activity is building houses
+        in The Sims.
+        <br />
+        This app was created using Next.js on the front end and the content is
+        managed from Contentful
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,34 @@
-import { createClient } from 'contentful'
+import { createClient } from "contentful";
+import BuildCard from "../components/BuildCard";
 
 export const getStaticProps = async () => {
-
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  })
+  });
 
-  const res = await client.getEntries({content_type: 'build'})
+  const res = await client.getEntries({ content_type: "build" });
   return {
     props: {
-      builds: res.items
-    }
-  }
-
-}
+      builds: res.items,
+    },
+  };
+};
 
 const Home = ({ builds }) => {
-  console.log(builds)
-  
-  const names = builds.map(build => {
-    return <p>{build.fields.name}</p>
-  })
+  console.log(builds);
 
-   return (
-     <div className="home">
-       <h1>Builds List</h1>
-       {names}
-     </div>
-   );
-  }
+  const cards = builds.map((build) => {
+    return <BuildCard key={build.sys.id} build={build} />;
+  });
 
-export default Home
+  return (
+    <div id="home">
+      <div className="card-container">
+        {cards}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
