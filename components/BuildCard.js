bb8faed621464f9574ade346dfff0f3dddacdd2a
mikeisfake <mikescooper@icloud.com>
2021-07-13 03:49:22 -0400

    updating cards on homepage to be full width.

--- components/BuildCard.js
+++ components/BuildCard.js
@@ -10,12 +10,12 @@ export default function BuildCard({ build }) {
         <div className="image">
           <img src={"https:" + coverImage.fields.file.url} alt={name} />
         </div>
-        <h4 className="name">{name}</h4>
         <div className="details">
+        <h4 className="name">{name}</h4>
           <p> {lotSize} </p>
           <p> {world} </p>
           <p> {lotType} </p>
-          <p> §{cost}.00 </p>
+          <p> §{cost.toLocaleString()}.00 </p>
         </div>
       </div>
     </Link>
