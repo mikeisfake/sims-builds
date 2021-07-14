fa041d12d3eb72a1bda04cdb8e5a7bb9d4870de7
mikeisfake <mikescooper@icloud.com>
2021-07-14 11:00:07 -0400

    prevent body from scolling when menu is open

--- functions/HandleMenu.js
+++ functions/HandleMenu.js
@@ -1,19 +1,23 @@
 export const openMenu = () => {
   const menu = document.querySelector("nav");
+  const body = document.querySelector("body");
   const cover = document.getElementById("cover");
   const list = document.getElementById("nav-list");
-
+  
   menu.classList.toggle("open");
+  body.classList.toggle("open");
   cover.classList.toggle("open");
   list.classList.toggle("open");
 };
 
 export const closeMenu = () => {
   const menu = document.querySelector("nav");
+  const body = document.querySelector("body");
   const cover = document.getElementById("cover");
   const list = document.getElementById("nav-list");
-
+  
   menu.classList.remove("open");
+  body.classList.remove("open");
   cover.classList.remove("open");
   list.classList.remove("open");
 };
