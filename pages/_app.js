a4e08d4f57f76a35bbea0e374827d665a688bc36
mikeisfake <mikescooper@icloud.com>
2021-07-16 12:41:00 -0400

    setting up fontawesome and library

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/app.scss'
 import Layout from '../components/Layout'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGlobeEurope, faBorderAll, faHome, faMoneyBill, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faGlobeEurope, faBorderAll, faHome, faMoneyBill, faChevronRight, faChevronLeft);
 
 function MyApp({ Component, pageProps }) {
   return (
